19955a2e479bc87d491ffb72c0abd98e14989d91
Abel Carreras <abelcarreras83@gmail.com>
2022-05-30 10:42:18 +0200

    added cache data example

---
+++ day_3/example_7/example_7a_2.py
@@ -0,0 +1,58 @@
+
+
+class Simulation:
+    def __init__(self, potential):
+        self._potential = potential
+        self._calculation = None
+
+    def set_positions(self, positions):
+        self._positions = positions
+
+    def set_velocites(self, velocities):
+        self._velocites = velocities
+
+    def set_masses(self, masses):
+        self._masses = masses
+
+    def calculate(self):
+        if self._calculation is None:
+            print('Not yet calculated, calculating now!')
+            kinetic = sum([0.5 * m * v**2 for m, v in zip(self._masses, self._velocites)])
+            potential = sum([self._potential(p) for p in self._positions])
+            self._calculation = kinetic + potential
+
+    def get_results(self):
+        if self._calculation is None:
+            self.calculate()
+        return self._calculation
+
+
+# my data
+positions = [1, 2, 3]
+velocities = [0, 1, -1]
+masses = [1, 1, 1]
+
+
+def potential(x):
+    return x ** 2
+
+
+simulation_1 = Simulation(potential)
+
+simulation_1.set_positions(positions)
+simulation_1.set_velocites(velocities)
+simulation_1.set_masses(masses)
+
+# simulation_1.calculate()
+
+result = simulation_1.get_results()
+print(result)
+
+result = simulation_1.get_results()
+print(result)
+
+result = simulation_1.get_results()
+print(result)
+
+
+
